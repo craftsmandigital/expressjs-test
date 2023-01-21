fb31b47a4618c13b765af892999e9cb824cc5b57
craftsmandigital <hackjack@tutanota.com>
2023-01-21 15:59:04 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+*Testing Express JS stuff
+
+**Inspiration sources
+Testing and exploring is heavily inspired of these websites:
+- https://www.sammeechward.com/connect-to-mysql-from-node
+- https://raddy.dev/blog/simple-user-management-system-nodejs-express-mysql-handlebars/
+- https://mozilla.github.io/nunjucks/templating.html
+
---
+++ app.js
@@ -0,0 +1,40 @@
+import express from 'express'
+import cors from 'cors'
+
+import { getNotes, getNote, createNote } from './database.js'
+
+const app = express()
+
+app.use(express.json())
+
+app.use(express.urlencoded({ extended: false }));
+
+app.use(cors());
+
+
+app.get("/notes", async (req, res) => {
+  const notes = await getNotes()
+  res.send('<h1> halla på deg </h1>'.concat(JSON.stringify(notes)))
+})
+
+app.get("/notes/:id", async (req, res) => {
+  const id = req.params.id
+  const note = await getNote(id)
+  res.send(note)
+})
+
+app.post("/notes", async (req, res) => {
+  const { title, contents } = req.body
+  const note = await createNote(title, contents)
+  res.status(201).send(note)
+})
+
+
+app.use((err, req, res, next) => {
+  console.error(err.stack)
+  res.status(500).send('Something broke 💩')
+})
+
+app.listen(8080, () => {
+  console.log('Server is running on port 8080')
+})
